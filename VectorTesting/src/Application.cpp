a99a60d2293d7f2835800d69470e88d03d9fecae
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2022-02-02 15:20:57 +0100

    First Tag Added

--- VectorTesting/src/Application.cpp
+++ VectorTesting/src/Application.cpp
@@ -3,13 +3,7 @@
 
 int main() 
 {
-	Vector playerPos(20, 20, 20);
-
-	printf("Pos x: %f, Pos y: %f, Pos z: %f\n",
-		static_cast<float>(playerPos[VectorAxis::AXIS_X]),
-		static_cast<float>(playerPos[VectorAxis::AXIS_Y]),
-		static_cast<float>(playerPos[VectorAxis::AXIS_Z])
-	);
+	printf("Hello world!");
 
 	system("pause");
 
