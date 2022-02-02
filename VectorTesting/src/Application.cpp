8c13ccea527c06a07ef204186978ceaf3c5929fb
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2022-02-02 16:04:42 +0100

    Added SFML Library in development

--- VectorTesting/src/Application.cpp
+++ VectorTesting/src/Application.cpp
@@ -1,17 +1,80 @@
+#include <SFML/Graphics.hpp>
+#include <string>
 #include <iostream>
-#include "VectorHelper.h"
 
-int main() 
+int main()
 {
-	Vector playerPos(20, 20, 20);
+    sf::RenderWindow window(sf::VideoMode(500, 500), "Testing SFML");
+    sf::RectangleShape shape(sf::Vector2f(0, 30));
+    sf::RectangleShape rectangle(sf::Vector2f(0, 30));
 
-	printf("Pos x: %f, Pos y: %f, Pos z: %f\n",
-		static_cast<float>(playerPos[VectorAxis::AXIS_X]),
-		static_cast<float>(playerPos[VectorAxis::AXIS_Y]),
-		static_cast<float>(playerPos[VectorAxis::AXIS_Z])
-	);
+    sf::CircleShape circle(200, 5);
 
-	system("pause");
+    sf::String string = "Weird font choice uh";
+    sf::Font font;
+    if (!font.loadFromFile("resources/fonts/Little Comet Demo Version.otf"))
+    {
+        printf("error!");
+    }
 
-	return 0;
+    sf::Text text(string, font, 50);
+    text.setPosition(sf::Vector2f(0, window.getPosition().y / 2));
+    sf::Texture txt;
+    if (txt.loadFromFile("resources/texture/texture.jpg"));
+    {
+        shape.setTexture(&txt, false);
+    }
+
+    rectangle.setFillColor(sf::Color::Red);
+    rectangle.setPosition(sf::Vector2f(shape.getSize().x, shape.getSize().y));
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+                printf("goodbye!");
+            }
+
+
+            if (event.type == sf::Event::KeyPressed)
+            {
+                printf("Key Pressed!\n");
+            }
+
+            if (event.type == sf::Event::Resized)
+                text.setPosition(sf::Vector2f(0, window.getPosition().y / 2));
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            {
+                if (event.KeyPressed)
+                {
+                    printf("Key Pressed!\n");
+                }
+            }
+        }
+
+        window.clear();
+
+        window.draw(shape);
+        window.draw(text);
+        window.draw(rectangle);
+        window.draw(circle);
+
+        circle.setRotation(circle.getRotation() + .1);
+
+        if (shape.getSize().x <= window.getSize().x)
+        {
+            shape.setSize(sf::Vector2f(shape.getSize().x + sf::Vector2f(.1, .1).x, shape.getSize().y));
+            rectangle.setSize(sf::Vector2f(rectangle.getSize().x + sf::Vector2f(.1, .1).x, rectangle.getSize().y));
+        }
+
+        window.display();
+    }
+
+    return 0;
 }
