aef65f5f9d6dec04157e0596282f7a581334544c
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2022-02-12 10:27:39 +0100

    Added Window Implementation with SFML

--- VectorTesting/src/Application.cpp
+++ VectorTesting/src/Application.cpp
@@ -4,13 +4,13 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(500, 500), "Testing SFML");
+    sf::RenderWindow window(sf::VideoMode(500, 500), "Game Window");
     sf::RectangleShape shape(sf::Vector2f(0, 30));
     sf::RectangleShape rectangle(sf::Vector2f(0, 30));
 
     sf::CircleShape circle(200, 5);
 
-    sf::String string = "Weird font choice uh";
+    sf::String string = "Window Implementation";
     sf::Font font;
 
     if (!font.loadFromFile("resources/fonts/Little Comet Demo Version.otf"))
@@ -20,12 +20,6 @@ int main()
 
     sf::Text text(string, font, 50);
     text.setPosition(sf::Vector2f(0, window.getPosition().y / 2));
-    sf::Texture texture;
-
-    if (texture.loadFromFile("resources/texture/texture.jpg"));
-    {
-        shape.setTexture(&texture, false);
-    }
 
     rectangle.setFillColor(sf::Color::Red);
     rectangle.setPosition(sf::Vector2f(shape.getSize().x, shape.getSize().y));
@@ -65,13 +59,7 @@ int main()
         window.draw(shape);
         window.draw(text);
         window.draw(rectangle);
-        window.draw(circle);
         
-        
-        printf("X size: %f, Y size: %f\n", window.getView().getCenter().x, window.getView().getCenter().y);
-        
-        circle.setRotation(circle.getRotation() + .1);
-
         if (shape.getSize().x <= window.getSize().x)
         {
             shape.setSize(sf::Vector2f(shape.getSize().x + sf::Vector2f(.1, .1).x, shape.getSize().y));
