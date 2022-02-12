0a2c923cd75373685af688af360991c225d3028e
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2022-02-12 14:33:26 +0100

    Added * and *= operators

--- VectorTesting/src/Application.cpp
+++ VectorTesting/src/Application.cpp
@@ -6,12 +6,12 @@ int main()
 	Vector playerPos(20,20,20);
 	Vector newPos(10,10,10);
 
-	Vector tempPos = playerPos + newPos;
+	playerPos *= 2;
 
 	printf("Player x: %f, Player y: %f, Player z: %f",
-		static_cast<float>(tempPos[VectorAxis::AXIS_X]),
-		static_cast<float>(tempPos[VectorAxis::AXIS_Y]),
-		static_cast<float>(tempPos[VectorAxis::AXIS_Z])
+		static_cast<float>(playerPos[VectorAxis::AXIS_X]),
+		static_cast<float>(playerPos[VectorAxis::AXIS_Y]),
+		static_cast<float>(playerPos[VectorAxis::AXIS_Z])
 		);
 
 
